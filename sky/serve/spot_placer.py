bc063db9225df507dcea7d505e4508e34b9e8847
MaoZiming <ziming.mao@yale.edu>
2023-10-24 15:37:02 -0700

    checkpt

--- sky/serve/spot_placer.py
+++ sky/serve/spot_placer.py
@@ -76,9 +76,8 @@ class SpotPlacer:
                                                      self.use_spot)
             while zone in self.preempted_zones or skymap_status == map_client.ProbeStatus.UNAVAILABLE:  # pylint: disable=line-too-long
                 zone = random.choice(self.zones)
-                skymap_status = self.skymap_client.probe(self.cloud, zone,
-                                                     self.accelerators,
-                                                     self.use_spot)
+                skymap_status = self.skymap_client.probe(
+                    self.cloud, zone, self.accelerators, self.use_spot)
                 time.sleep(1)
             logger.info(f'self.preempted_zones: {self.preempted_zones}')
         logger.info(f'Chosen zone: {zone}, policy: {self.spot_policy}')
